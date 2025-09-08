83263e98e2db40ee07afb932ebc46bbfb8725138
maryam aalizadeh <maalizadeh7394@gmail.com>
2025-09-08 14:18:23 +0430

    first commit

---
+++ app/movies/[id].tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const MovieDeatils = () => {
+  return (
+    <View>
+      <Text>MovieDeatils</Text>
+    </View>
+  );
+};
+
+export default MovieDeatils;
